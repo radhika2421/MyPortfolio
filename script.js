892b2ef1d3979b80c4aa2cc59e3f6eb5c8d608e0
Rads <radhika2changia@gmail.com>
2025-06-08 13:36:44 +0530

    color updated

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 let isDark = false;
 
 function changeTheme() {
-  const themeLink = document.getElementById("theme-style");
-  const sunIcon = document.getElementById("sun-icon");
-  const moonIcon = document.getElementById("moon-icon");
+const themeLink = document.getElementById("theme-style");
+const sunIcon = document.getElementById("sun-icon");
+const moonIcon = document.getElementById("moon-icon");
 
-  if (isDark) {
-    // Switch to light
-    themeLink.setAttribute("href", "style.css");
-    sunIcon.style.display = "none";
-    moonIcon.style.display = "inline";
-  } else {
-    // Switch to dark
-    themeLink.setAttribute("href", "dark-mode.css");
-    sunIcon.style.display = "inline";
-    moonIcon.style.display = "none";
-  }
+if (isDark) {
+// Switch to light
+themeLink.setAttribute("href", "style.css");
+sunIcon.style.display = "none";
+moonIcon.style.display = "inline";
+} else {
+// Switch to dark
+themeLink.setAttribute("href", "dark-mode.css");
+sunIcon.style.display = "inline";
+moonIcon.style.display = "none";
+}
 
-  isDark = !isDark;
+isDark = !isDark;
 }
